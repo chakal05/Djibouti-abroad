1b21d913befcdbef1e8ea0447311eecc20abf8ca
chakal05 <chakirrasta@gmail.com>
2020-12-04 11:04:45 +0100

    test 2

--- public/precache-manifest.c9ca60b3c39a2d4149132e060e67c07e.js
+++ public/precache-manifest.c9ca60b3c39a2d4149132e060e67c07e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c33e990e0215f36d1c0e8638b58f6efd",
+    "revision": "0f3839f2c245a12736644096454ee390",
     "url": "/index.html"
   },
   {
-    "revision": "005902e8b48533ae071f",
+    "revision": "fb4ff51376710630a00a",
     "url": "/static/css/main.d93dc9d5.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b660ef73.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "005902e8b48533ae071f",
-    "url": "/static/js/main.8d88e076.chunk.js"
+    "revision": "fb4ff51376710630a00a",
+    "url": "/static/js/main.eb6902e0.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
